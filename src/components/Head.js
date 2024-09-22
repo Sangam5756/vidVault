d7db49ec02fdce482341d0d95c87e6be629d235a
sangam5756 <sangammunde3@gmail.com>
2024-09-22 10:51:46 +0530

    Project-Setup & tailwindcss & header & sidebar done

---
+++ src/components/Head.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div className="grid grid-flow-col p-5 shadow-lg items-center ">
+      {/* first section logo+menu */}
+      <div className="col-span-1 flex ">
+        <img
+          className="h-8"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0evWy6vmua96UkF8RqHQv-SoBcuu3V9fwZw&s"
+          alt="menu"
+        />
+        <img
+          className="h-8 px-2"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+          alt="youtube-logo"
+        />
+      </div>
+
+      {/* Search  */}
+      <div className="col-span-10 ">
+        <input
+          className="w-1/2   py-1 ml-32 rounded-l-full outline-none border-gray-500 border px-4 "
+          type="text"
+        />
+        <button className="border py-1 rounded-r-full bg-gray-100 border-gray-500 px-2">
+          <i className="fa fa-search w-5"></i>
+        </button>
+      </div>
+
+      {/* user-icon */}
+      <div>
+        <img
+          className="h-8 col-span-1"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="user-icon"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
